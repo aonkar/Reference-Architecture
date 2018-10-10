b9ee66000ee97cb87fcb57aa33f3895ada9ea6e2
abhishek-onkar <abhishek.onkar60@gmail.com>
2018-10-10 13:00:56 +0530

    scafolding App

---
+++ Reference-architecture/src/main/java/com/refarch/Referencearchitecture/ReferenceArchitectureApplication.java
@@ -0,0 +1,12 @@
+package com.refarch.Referencearchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferenceArchitectureApplication.class, args);
+	}
+}
